4449291e53d977d7dae3de793cba750d9e9a77a0
MakTheDon69 <tomystark0077@gmail.com>
2024-10-07 22:35:35 -0400

    Update cities.py

--- solution_files/chapter_08/cities.py
+++ solution_files/chapter_08/cities.py
@@ -1,6 +1,6 @@
 def describe_city(city, country='chile'):
     """Describe a city."""
-    msg = f"{city.title()} is in {country.title()}."
+    msg = f'"{city.title()} is in {country.title()}."'
     print(msg)
 
 describe_city('santiago')
